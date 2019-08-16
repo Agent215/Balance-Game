e87e918c486bfeb01d884f7b7ff7ffb2c9250909
Brahm <abraham.schultz@gmail.com>
2019-08-16 12:09:52 -0400

    changed graphics
    changed graphics and adjusted collision detection of legs to change dificulty

---
+++ BalancingGuy/Assets/Scripts/VersionText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VersionText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Text>().text = "Application Version : " + Application.version;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
